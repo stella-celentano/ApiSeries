d9e779265049f65a7bcb0a3749d6f5ad46cac008
stella-celentano <stellacelentano99@gmail.com>
2020-02-15 16:01:56 -0300

    Alteração dos status e mensagens de Erro e Sucesso das requisições

--- src/controllers/series.js
+++ src/controllers/series.js
@@ -4,47 +4,47 @@ const SerieModel = require('./../models/series')
 
 class Series {
     get(req, res) {
-        SerieModel.find({}, (error, serie) => {
-            if (error)
-                res.send(error)
+        SerieModel.find({}, (err, serie) => {
+            if (err)
+                res.status(500).json({ message: 'Erro ao processar requisição', error: err })
             else
-                res.json({ data: serie })
+                res.status(200).json({ message: 'Dados recuperados com sucesso', data: serie })
         })
     }
 
     getById(req, res) {
-        SerieModel.findById(req.params.id, (error, serie) => {
-            if (error)
-                res.send(error)
+        SerieModel.findById(req.params.id, (err, serie) => {
+            if (err)
+                res.status(500).json({ message: 'Erro ao processar requisição', error: err })
             else
-                res.json({ data: serie })
+                res.status(200).json({ message: 'Dados recuperados com sucesso', data: serie })
         })
     }
 
     create(req, res) {
-        SerieModel.create(req.body, (error, serie) => {
-            if (error)
-                res.send(error)
+        SerieModel.create(req.body, (err, serie) => {
+            if (err)
+                res.status(500).json({ message: 'Erro ao processar requisição', error: err })
             else
-                res.json({message: 'Successfully added series', data: serie})
+                res.status(200).json({ message: 'Série inserida com sucesso', data: serie })
         })
     }
 
-    update(req, res){
-        SerieModel.updateOne({_id: req.params.id}, {$set: req.body}, (error, serie) => {
-            if (error)
-               res.send(error)
+    update(req, res) {
+        SerieModel.updateOne({ _id: req.params.id }, { $set: req.body }, (err, serie) => {
+            if (err)
+                res.status(500).json({ message: 'Erro ao processar requisição', error: err })
             else
-                res.json({message: 'Successfully updated series', data: serie}) 
+                res.status(200).json({ message: 'Série atualizada com sucesso', data: serie })
         })
     }
 
-    delete(req, res){
-        SerieModel.deleteOne({_id: req.params.id}, (error, serie) => {
-            if(error)
-                res.send(error)
-            else    
-                res.json({message: 'Series deleted successfully', data: serie})
+    delete(req, res) {
+        SerieModel.deleteOne({ _id: req.params.id }, (err, serie) => {
+            if (err)
+            res.status(500).json({ message: 'Erro ao processar requisição', error: err })
+        else
+            res.status(200).json({ message: 'Série apagada com sucesso', data: serie })
         })
     }
 }
